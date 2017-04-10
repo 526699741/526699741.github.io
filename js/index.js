2e057f9a636d12a1514ee04a6c1f473592d78f36
ElementById get <ElementById get>
2017-04-10 15:35:04 +0800

    v1.0.1

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2017/4/10.
+ */
+window.onload=function(){
+    var vm=new Vue({
+        el:'body',
+        data:{
+            head:[
+                {href:'javascript:;',cont:'首页'},
+                {href:'html/project/index.html',cont:'关于我'},
+                {href:'javascript:;',cont:'作品'},
+                {href:'javascript:;',cont:'技能'},
+                {href:'javascript:;',cont:'联系我'}
+            ]
+        },
+        methods:{
+            show:function () {
+
+            }
+        }
+    });
+
+};
+
+
+
+
+
+
+
