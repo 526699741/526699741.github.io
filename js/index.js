9a02b6847af0eaba65c2fb45f163dfa28ede58df
ElementById get <ElementById get>
2017-04-13 21:59:36 +0800

    v1.0.2

--- js/index.js
+++ js/index.js
@@ -1,26 +1,56 @@
 /**
  * Created by Administrator on 2017/4/10.
  */
-window.onload=function(){
-    var vm=new Vue({
-        el:'body',
-        data:{
-            head:[
-                {href:'javascript:;',cont:'首页'},
-                {href:'html/project/index.html',cont:'关于我'},
-                {href:'javascript:;',cont:'作品'},
-                {href:'javascript:;',cont:'技能'},
-                {href:'javascript:;',cont:'联系我'}
-            ]
-        },
-        methods:{
-            show:function () {
 
-            }
+new Vue({
+    el:'#app',
+    data:{
+        "top":[
+            {"href":"html/project/index.html","cont":"首页"},
+            {"href":"html/project/index.html","cont":"关于我"},
+            {"href":"html/project/index.html","cont":"作品"},
+            {"href":"html/project/index.html","cont":"技能"},
+            {"href":"html/project/index.html","cont":"联系我"}
+        
+        ],
+        "swiper-slide":[
+            {},
+            {},
+            {},
+            {}
+        ]
+    },
+    methods:{
+        show:function () {
+
         }
-    });
+    }
+});
 
-};
+new Vue({
+    el:'#app2',
+    data:{
+        "content":[
+            {"title":"一句话"},
+            {"title":"name 0aaa"},
+            {"title":"求职 0前端工程师"},
+            {"title":"工作地点 0 北京"},
+            {"title":"tel 0 15130020505"},
+            {"title":"email 0 526699741@qq.com"}
+        ]
+    }
+});
+
+//swiper .js
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    direction: 'vertical',
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 30,
+    mousewheelControl: true
+});
 
 
 
