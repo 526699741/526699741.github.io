ed6ecb3bca9daa01c19801dc9bc945f2f2f36b5c
ElementById get <ElementById get>
2017-04-23 18:37:36 +0800

    yipao

--- js/index.js
+++ js/index.js
@@ -3,15 +3,23 @@
  */
 
 new Vue({
-    el:'#app',
+    el:'.swiper-container',
     data:{
         "top":[
-            {"href":"html/project/index.html","cont":"首页"},
+            {"href":"#cont2","cont":"首页"},
             {"href":"html/project/index.html","cont":"关于我"},
             {"href":"html/project/index.html","cont":"作品"},
             {"href":"html/project/index.html","cont":"技能"},
             {"href":"html/project/index.html","cont":"联系我"}
         
+        ],
+        "content":[
+            {"title":"『一句话』"},
+            {"title":"name : aa"},
+            {"title":"求职 : 前端工程师"},
+            {"title":"工作地点 : 北京"},
+            {"title":"tel : 15130020505"},
+            {"title":"email : 526699741@qq.com"}
         ],
         "swiper-slide":[
             {},
@@ -21,25 +29,13 @@ new Vue({
         ]
     },
     methods:{
-        show:function () {
-
-        }
+        show:function () {}
     }
 });
 
-new Vue({
-    el:'#app2',
-    data:{
-        "content":[
-            {"title":"一句话"},
-            {"title":"name 0aaa"},
-            {"title":"求职 0前端工程师"},
-            {"title":"工作地点 0 北京"},
-            {"title":"tel 0 15130020505"},
-            {"title":"email 0 526699741@qq.com"}
-        ]
-    }
-});
+
+
+
 
 //swiper .js
 
